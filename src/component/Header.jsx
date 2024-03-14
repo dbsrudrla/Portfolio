9932744cc92f4340318848cbdbc6a560ccbc46d8
dbsrudrla <rladbsrud011120@naver.com>
2024-03-14 22:38:11 +0900

    fix : remove warning

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import styled from "styled-components";
 import Colors from "../common/Color";
 import { FaBars } from "react-icons/fa";
